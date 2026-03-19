9584c16a08928774a90e50a8bd54f158dcd8fddb
Pedro Lajas <pedrolajas.gamedesigner@gmail.com>
2026-03-19 16:50:41 +0000

    Create Methods

---
+++ DungeonStats/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace DungeonStats
+{
+    public class Program
+    {
+        private static int Damage(int attack, int defense);
+        {
+            int stat = attack - defense;
+
+            if (stat > 0)
+            {
+                return stat;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+
